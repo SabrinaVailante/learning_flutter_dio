74daeb235c4b507c012f666d8f7aeacce1835066
Sabrina Vailante <sabrinavailante@hotmail.com>
2023-11-19 20:23:34 -0300

    Adcionando o Drawer.

--- lib/funcionalidades/views/pagina_principal.dart
+++ lib/funcionalidades/views/pagina_principal.dart
@@ -11,7 +11,41 @@ class _PaginaPrincipalState extends State<PaginaPrincipal> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Meu App")),
+      appBar: AppBar(title: const Text("Pagina Principal")),
+      drawer: Drawer(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              InkWell(
+                  child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      width: double.infinity,
+                      child: Text("Dados Cadastrais")),
+                  onTap: () {}),
+              Divider(color: Colors.deepOrangeAccent),
+
+              InkWell(
+                  child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      width: double.infinity,
+                      child: Text("Termos de Uso")),
+                  onTap: () {}),
+              Divider(color: Colors.deepOrangeAccent),
+
+              InkWell(
+                  child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      width: double.infinity,
+                      child: Text("configurações")),
+                  onTap: () {}),
+              Divider(color: Colors.deepOrangeAccent),
+
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
